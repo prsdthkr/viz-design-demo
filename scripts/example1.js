11b624a50a05198c60ef2eb0f6f998a4e0f9ec7d
Prasad Thakur <prasad.thakur@sap.com>
2020-02-25 01:30:26 -0800

    example1, title, desc

--- scripts/example1.js
+++ scripts/example1.js
@@ -3,7 +3,7 @@
 
 var margin = { top: 20, right: 20, bottom: 30, left: 40 },
     width = 1700 - margin.left - margin.right,
-    height = 750 - margin.top - margin.bottom;
+    height = 550 - margin.top - margin.bottom;
 
 var x0 = d3.scaleBand().range([0, width]);
 
@@ -25,6 +25,7 @@ var color = d3.scaleOrdinal().range(
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
+    .style("padding", '0 1em')
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
