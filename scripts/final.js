66cee882b64138f3cbfa5a5a6730fdfbf5133919
Prasad Thakur <prasad.thakur@sap.com>
2020-03-06 11:46:47 -0800

    info, formatting

--- scripts/final.js
+++ scripts/final.js
@@ -6,6 +6,30 @@
 
 // })
 
+
+//shamelessly copy pasted from stackoverflow:
+//https://stackoverflow.com/questions/9461621/format-a-number-as-2-5k-if-a-thousand-or-more-otherwise-900
+//author:https://stackoverflow.com/users/87015/salman-a
+function nFormatter(num, digits) {
+    var si = [
+        { value: 1, symbol: "" },
+        { value: 1E3, symbol: "k" },
+        { value: 1E6, symbol: "M" },
+        { value: 1E9, symbol: "G" },
+        { value: 1E12, symbol: "T" },
+        { value: 1E15, symbol: "P" },
+        { value: 1E18, symbol: "E" }
+    ];
+    var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    var i;
+    for (i = si.length - 1; i > 0; i--) {
+        if (num >= si[i].value) {
+            break;
+        }
+    }
+    return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+}
+
 const gSelector = (ABBR) => {
     return 'g#' + ABBR;
 }
@@ -38,26 +62,55 @@ window.onload = function () {
         d.forEach(element => {
             // console.log(element);
             const ABBR = element['Abr.'];
+            const communitySize = element['Community'];
             const two15 = float(element['2015 charges']);
             const two16 = float(element['2016 charges']);
             const two17 = float(element['2017 charges']);
             const avgRate = (two15 + two16 + two17) * 100 / 3;
-            console.log(ABBR, avgRate);
             if (!ABBR) return;
             selectStateG(ABBR)
                 .append("text")
                 .attr('fill', 'black')
                 .attr('y', center(ABBR, 'y'))
-                .attr('x', leftEdge(ABBR, 'x', 10))
+                .attr('x', rightEdge(ABBR, 'x', 10))
                 .attr('font-size', '20px')
                 .text(ABBR)
+
+            const tileWidth = getAtt(ABBR, 'width');
+            console.log(ABBR, tileWidth);
+
+            //render outside
+            let communitySizeY = center(ABBR, 'y', 15);
+            let communitySizeX = rightEdge(ABBR, 'x', 10);
+            let communityFontSize = '12px';
+
+            //render inside
+            var fontScale = d3.scale.linear().domain([1194000, 131000])
+                .range([80, 12]);
+            const leftPadding = 5;
+            if (int(tileWidth) >= 28 + leftPadding) {
+                communityFontSize = getFontSize(fontScale, communitySize);
+                const fontOffset = fontScale(int(communitySize));
+                communitySizeY = int(getAtt(ABBR, 'y')) + fontOffset;
+                communitySizeX = int(getAtt(ABBR, 'x')) + leftPadding;
+            }
+
             selectStateG(ABBR)
                 .append("text")
                 .attr('fill', 'black')
-                .attr('y', center(ABBR, 'y', 20))
-                .attr('x', leftEdge(ABBR, 'x', 10))
+                .attr('y', communitySizeY)
+                .attr('x', communitySizeX)
+                .attr('font-size', communityFontSize)
+                .text(nFormatter(communitySize, 2));
+
+            selectStateG(ABBR)
+                .append("text")
+                .attr('fill', 'black')
+                .attr('y', center(ABBR, 'y', 30))
+                .attr('x', rightEdge(ABBR, 'x', 10))
                 .attr('font-size', '12px')
                 .text(avgRate.toFixed(2) + '%');
+
 
 
             var greenScale = d3.scale.linear().domain([21, 37])
@@ -69,11 +122,13 @@ window.onload = function () {
         });
     })
 };
-
-function center(ABBR, cord, offset = 0) {
-    return int(getAtt(ABBR, cord)) + int(getAtt(ABBR, 'width')) / 2 + offset;
+function getFontSize(fontScale, domainVal) {
+    return fontScale(int(domainVal)) + 'px';
+};
+function center(ABBR, cord, offset = 0, selfWidth = 0) {
+    return int(getAtt(ABBR, cord)) + int(getAtt(ABBR, 'width')) / 2 - selfWidth / 2 + offset;
 }
-function leftEdge(ABBR, cord, offset = 0) {
+function rightEdge(ABBR, cord, offset = 0) {
     return int(getAtt(ABBR, cord)) + int(getAtt(ABBR, 'width')) + offset;
 }
 function int(val) {
