8c7b274143e53303d087e7bbbbab056c11ea16e8
Prasad Thakur <prasad.thakur@sap.com>
2020-03-12 00:10:44 -0700

    improvements

---
+++ scripts/final-2.js
@@ -0,0 +1,311 @@
+// d3.squareMap.setAttr({
+//     colorSet: 'Greys',
+//     labelStyle: 'abbr'
+// }).render('data/size-of-communities.csv', '.map-container');
+// $(window).on("load", function () {
+
+// })
+
+
+//shamelessly copy pasted from stackoverflow:
+//https://stackoverflow.com/questions/9461621/format-a-number-as-2-5k-if-a-thousand-or-more-otherwise-900
+//author:https://stackoverflow.com/users/87015/salman-a
+function nFormatter(num, digits) {
+    var si = [
+        { value: 1, symbol: "" },
+        { value: 1E3, symbol: "k" },
+        { value: 1E6, symbol: "M" },
+        { value: 1E9, symbol: "G" },
+        { value: 1E12, symbol: "T" },
+        { value: 1E15, symbol: "P" },
+        { value: 1E18, symbol: "E" }
+    ];
+    var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    var i;
+    for (i = si.length - 1; i > 0; i--) {
+        if (num >= si[i].value) {
+            break;
+        }
+    }
+    return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+}
+
+const gSelector = (ABBR) => {
+    return 'g#' + ABBR;
+}
+
+const rectSelector = (ABBR) => {
+    return 'rect#' + ABBR + '_2';
+}
+
+const selectStateRect = (ABBR) => {
+    return d3.select(rectSelector(ABBR))
+}
+
+const selectStateG = (ABBR) => {
+    return d3.select(gSelector(ABBR))
+}
+
+const getAtt = function (ABBR, key) {
+    if (!ABBR) return;
+    let val = selectStateRect(ABBR).attr(key);
+    return val ? val : 0;
+};
+
+const setAtt = function (ABBR, key, value) {
+    if (!ABBR) return;
+    selectStateRect(ABBR).attr(key, value);
+};
+window.onload = function () {
+    d3.json('data/states.json', function (sd) {
+        console.log('states', Object.entries(sd));
+
+        // d3.select('#statesRef')
+        //     .selectAll('li')
+        //     .data(Object.entries(sd))
+        //     .enter()
+        //     .append('li')
+        //     .attr('class', 'abr-li')
+        //     .html(d => d[0] + ":" + d[1]);
+
+        d3.csv('data/rich-size-of-communities-2.csv', function (d) {
+            d.forEach(element => {
+                // console.log(element);
+                const ABBR = element['Abr.'];
+                const communitySize = element['Community'];
+                const two15 = float(element['2015 charges']);
+                const two16 = float(element['2016 charges']);
+                const two17 = float(element['2017 charges']);
+                const percentInc = float(element['Percent Incidents']);
+                const avgRate = (two15 + two16 + two17) * 100 / 3;
+                if (!ABBR) return;
+
+                // console.log('percent yall', element['Percent Incidents']);
+
+                //placement calculations
+                const outThreshold = 30;
+                const tileWidth = getAtt(ABBR, 'width');
+                const tileHeight = getAtt(ABBR, 'height');
+                const tileY = int(getAtt(ABBR, 'y'));
+                const tileX = int(getAtt(ABBR, 'x'));
+
+
+                //render outside tile?
+                let communitySizeY = center(ABBR, 'y', 15);
+                let communitySizeX = rightEdge(ABBR, 'x', 10);
+                let communityFontSize = '12px';
+
+
+                let stateAbrY = center(ABBR, 'y');
+                let stateAbrX = communitySizeX;
+                const abrFontSize = '20px'
+
+
+                let percentY = center(ABBR, 'y', 30);
+                let percentX = communitySizeX;
+                const percentFontSize = '12px'
+
+
+                //render inside tile?
+                const fontScale = d3.scale.linear().domain([1194000, 131000])
+                    .range([80, 12]);
+                const leftPadding = 5;
+
+                if (int(tileWidth) >= outThreshold + leftPadding) {
+                    const leftMargin = tileX + leftPadding;
+
+                    const stateAbrFontOffset = pixelToNumber(abrFontSize);
+                    stateAbrY = tileY + stateAbrFontOffset;
+                    stateAbrX = leftMargin;
+
+                    communityFontSize = getFontSize(fontScale, communitySize);
+                    const fontOffset = fontScale(int(communitySize));
+                    communitySizeY = stateAbrY + fontOffset;
+                    communitySizeX = leftMargin;
+
+                    const percentFontOffset = pixelToNumber(abrFontSize);
+                    percentY = communitySizeY + percentFontOffset;
+                    percentX = leftMargin;
+                }
+
+
+                //render
+
+                selectStateG(ABBR)
+                    .data([element])
+                    .attr('class', 'state-tile-ctr')
+                    .on('mouseover', handleMouseOverRect)
+                    .on('mouseout', handleMouseOutRect)
+                selectStateRect(ABBR)
+                    .attr('class', 'state-tile')
+
+                //CA
+                selectStateG(ABBR)
+                    .append("text")
+                    .attr('fill', 'black')
+                    .attr('y', stateAbrY)
+                    .attr('x', stateAbrX)
+                    .attr('font-size', abrFontSize)
+                    .text(ABBR)
+
+                //1.19M
+                selectStateG(ABBR)
+                    .append("text")
+                    .attr('fill', 'black')
+                    .attr('y', communitySizeY)
+                    .attr('x', communitySizeX)
+                    .attr('font-size', communityFontSize)
+                    .text(nFormatter(communitySize, 2));
+
+                //27.67%
+                // selectStateG(ABBR)
+                //     .append("text")
+                //     .attr('fill', 'black')
+                //     .attr('y', percentY)
+                //     .attr('x', percentX)
+                //     .attr('font-size', percentFontSize)
+                //     .text(percentInc.toFixed(2) + '%');
+
+                // var greenScale = d3.scale.linear().domain([0.054285714, 1.959574468])
+                // .range(["#13532e", "#2ecc71"]); //dark green > light
+                // var redScale = d3.scale.linear().domain([0.157777778, 3.443589744])
+                // .range(["#ffccc7", "#b51200"]); //light red > dark
+                // const colorScale = element.Legislation === 'No' ? redScale : greenScale;
+                // setAtt(ABBR, 'fill', colorScale(percentInc));
+                setAtt(ABBR, 'fill', getFill(element));
+                setAtt(ABBR, 'class', (element.Legislation === 'Yes' ? 'green-rect' : 'red-rect') + ' rect-stroke');
+            });
+
+            //retrieved from https://bl.ocks.org/hrecht/f84012ee860cb4da66331f18d588eee3
+            let descriminationData = d;
+            //sort bars based on value
+            descriminationData = descriminationData.sort(function (state1, state2) {
+                return d3.ascending(state1['Percent Incidents'], state2['Percent Incidents']);
+            });
+
+            descriminationData.forEach(state => {
+                console.log(state['State'], state['Percent Incidents']);
+            });
+
+            //set up svg using margin conventions - we'll need plenty of room on the left for labels
+            var margin = {
+                top: 50,
+                right: 50,
+                bottom: 15,
+                left: 150
+            };
+
+            var width = 1000 - margin.left - margin.right,
+                height = 2000 - margin.top - margin.bottom;
+
+            var svg = d3.select("#graphic").append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            var xFunc = d3.scale.linear()
+                .range([0, width])
+                .domain([0, d3.max(descriminationData, function (state) {
+                    return state['Percent Incidents'];
+                })]);
+
+            var yFunc = d3.scale.ordinal()
+                .rangeRoundBands([height, 0], .1)
+                .domain(descriminationData.map(function (state) {
+                    return state['State'];
+                }));
+
+            var yLabelFunc = d3.scale.ordinal()
+                .rangeRoundBands([height, 0], .1)
+                .domain(descriminationData.map(function (state) {
+                    return state['Abr.'] + ', ' + state['State'];
+                }));
+
+            //make y axis to show bar names
+            var yAxis = d3.svg.axis()
+                .scale(yLabelFunc)
+                //no tick marks
+                .tickSize(1)
+                .orient("left");
+
+            var gy = svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+
+            var bars = svg.selectAll(".bar")
+                .data(descriminationData)
+                .enter()
+                .append("g")
+                .attr('fill', (state) => getFill(state))
+
+            //append rects
+            bars.append("rect")
+                .attr("class", "bar")
+                .attr("y", function (d) {
+                    let yValue = yFunc(d['State']);
+                    return yValue;
+                })
+                .attr("height", yFunc.rangeBand())
+                .attr("x", 0)
+                .attr("width", function (d) {
+                    return xFunc(d['Percent Incidents']);
+                });
+
+            //add a value label to the right of each bar
+            bars.append("text")
+                .attr("class", "label")
+                //y position of the label is halfway down the bar
+                .attr("y", function (state) {
+                    return yFunc(state['State']) + yFunc.rangeBand() / 2 + 4;
+                })
+                //x position is 3 pixels to the right of the bar
+                .attr("x", function (state) {
+                    return xFunc(state['Percent Incidents']) + 3;
+                })
+                .text(function (state) {
+                    return float(state['Percent Incidents']).toFixed(2) + '%';
+                })
+                .attr('fill', 'black');
+
+        })
+    });
+};
+
+function getFill(element) {
+    return element.Legislation === 'Yes' ? '#27ae60' : '#c0392b';
+}
+
+function handleMouseOverRect(d, i) {
+    let filter = 'sofGlowRed';
+    if (d && d.Legislation === 'Yes') {
+        filter = 'sofGlowGreen';
+    }
+    this && this.setAttribute('filter', 'url(#' + filter + ')');
+}
+
+function handleMouseOutRect(d, i) {
+    this && this.removeAttribute('filter');
+}
+
+
+function pixelToNumber(str) {
+    return str ? int(str.split('px')[0]) : 0;
+}
+
+function getFontSize(fontScale, domainVal) {
+    return fontScale(int(domainVal)) + 'px';
+};
+function center(ABBR, cord, offset = 0, selfWidth = 0) {
+    return int(getAtt(ABBR, cord)) + int(getAtt(ABBR, 'width')) / 2 - selfWidth / 2 + offset;
+}
+function rightEdge(ABBR, cord, offset = 0) {
+    return int(getAtt(ABBR, cord)) + int(getAtt(ABBR, 'width')) + offset;
+}
+function int(val) {
+    return Number.parseInt(val);
+}
+function float(val) {
+    return Number.parseFloat(val);
+}
+
